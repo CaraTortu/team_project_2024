d2594a235781bdaeff8af8d4d2520b49a24acdea
CaraTortu <javierdj@jadk.com>
2024-02-16 10:29:09 +0000

    Add docker building

--- Project/compile_docker_image.sh
+++ Project/compile_docker_image.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Build the JAR file
-cd gp-system
-mvn package
-mv target/*.jar ../docker_files/gp-system.jar
-
 # Compile the docker image
-cd ..
 docker build -t gp_system .
 
--- Project/gp-system/next.config.js
+++ Project/gp-system/next.config.js
@@ -5,6 +5,10 @@
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+    output: "standalone",
+    reactStrictMode: true,
+    swcMinify: true
+};
 
 export default config;
