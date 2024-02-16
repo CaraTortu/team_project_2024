25749259e5c2abd7ae708b9244b0f3ef2549f8be
CaraTortu <javierdj@jadk.com>
2024-02-16 10:48:26 +0000

    Fix dockerfile

--- Project/docker_files/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ ! -e /opt/app/*.jar ]
-    then
-        echo "ERROR: Something went wrong and the java file does not exist."
-        exit
-fi
-
-java -jar /opt/app/gp-system.jar
