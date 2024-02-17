4d40a147d3dfa8299b7d9a08e160720650f4d2a2
CaraTortu <javierdj@jadk.com>
2024-02-17 12:13:59 +0000

    4 space indexing & DB schema

--- Project/gp-system/src/server/api/trpc.ts
+++ Project/gp-system/src/server/api/trpc.ts
@@ -6,27 +6,29 @@ import { getServerAuthSession } from "~/server/auth";
 import { db } from "~/server/db";
 
 export const createTRPCContext = async (opts: { headers: Headers }) => {
-  const session = await getServerAuthSession();
+    const session = await getServerAuthSession();
 
-  return {
-    db,
-    session,
-    ...opts,
-  };
+    return {
+        db,
+        session,
+        ...opts,
+    };
 };
 
 const t = initTRPC.context<typeof createTRPCContext>().create({
-  transformer: superjson,
-  errorFormatter({ shape, error }) {
-    return {
-      ...shape,
-      data: {
-        ...shape.data,
-        zodError:
-          error.cause instanceof ZodError ? error.cause.flatten() : null,
-      },
-    };
-  },
+    transformer: superjson,
+    errorFormatter({ shape, error }) {
+        return {
+            ...shape,
+            data: {
+                ...shape.data,
+                zodError:
+                    error.cause instanceof ZodError
+                        ? error.cause.flatten()
+                        : null,
+            },
+        };
+    },
 });
 
 export const createTRPCRouter = t.router;
@@ -50,13 +52,13 @@ export const publicProcedure = t.procedure;
  */
 
 export const protectedProcedure = t.procedure.use(({ ctx, next }) => {
-  if (!ctx.session || !ctx.session.user) {
-    throw new TRPCError({ code: "UNAUTHORIZED" });
-  }
-  return next({
-    ctx: {
-      // infers the `session` as non-nullable
-      session: { ...ctx.session, user: ctx.session.user },
-    },
-  });
+    if (!ctx.session || !ctx.session.user) {
+        throw new TRPCError({ code: "UNAUTHORIZED" });
+    }
+    return next({
+        ctx: {
+            // infers the `session` as non-nullable
+            session: { ...ctx.session, user: ctx.session.user },
+        },
+    });
 });
--- Project/gp-system/src/server/db/schema.ts
+++ Project/gp-system/src/server/db/schema.ts
@@ -1,116 +1,127 @@
 import { relations, sql } from "drizzle-orm";
 import {
-  index,
-  integer,
-  pgTableCreator,
-  primaryKey,
-  serial,
-  text,
-  timestamp,
-  varchar,
+    boolean,
+    date,
+    index,
+    integer,
+    pgEnum,
+    pgTableCreator,
+    primaryKey,
+    real,
+    serial,
+    text,
+    timestamp,
+    varchar,
 } from "drizzle-orm/pg-core";
 import { type AdapterAccount } from "next-auth/adapters";
 
-/**
- * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
- * database instance for multiple projects.
- *
- * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
- */
 export const createTable = pgTableCreator((name) => `gp-system_${name}`);
 
-export const posts = createTable(
-  "post",
-  {
-    id: serial("id").primaryKey(),
-    name: varchar("name", { length: 256 }),
-    createdById: varchar("createdById", { length: 255 })
-      .notNull()
-      .references(() => users.id),
-    createdAt: timestamp("created_at")
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: timestamp("updatedAt"),
-  },
-  (example) => ({
-    createdByIdIdx: index("createdById_idx").on(example.createdById),
-    nameIndex: index("name_idx").on(example.name),
-  })
-);
+export const userTypeEnum = pgEnum("userType", ["doctor", "frontdesk", "user"]);
+export const paymentStatus = pgEnum("paymentStatus", ["complete", "failed", "pending"]);
 
 export const users = createTable("user", {
-  id: varchar("id", { length: 255 }).notNull().primaryKey(),
-  name: varchar("name", { length: 255 }),
-  email: varchar("email", { length: 255 }).notNull(),
-  emailVerified: timestamp("emailVerified", {
-    mode: "date",
-  }).default(sql`CURRENT_TIMESTAMP`),
-  image: varchar("image", { length: 255 }),
+    id: varchar("id", { length: 255 }).notNull().primaryKey(),
+    name: varchar("name", { length: 255 }),
+    email: varchar("email", { length: 255 }).notNull(),
+    emailVerified: timestamp("emailVerified", {
+        mode: "date",
+    }).default(sql`CURRENT_TIMESTAMP`),
+    image: varchar("image", { length: 255 }),
+    usertype: userTypeEnum("userType").default("user"),
+    doctorId: varchar("doctorId", { length: 255 }).default(sql`NULL`)
 });
 
 export const usersRelations = relations(users, ({ many }) => ({
-  accounts: many(accounts),
+    accounts: many(accounts),
+    appointments: many(appointment)
 }));
 
 export const accounts = createTable(
-  "account",
-  {
-    userId: varchar("userId", { length: 255 })
-      .notNull()
-      .references(() => users.id),
-    type: varchar("type", { length: 255 })
-      .$type<AdapterAccount["type"]>()
-      .notNull(),
-    provider: varchar("provider", { length: 255 }).notNull(),
-    providerAccountId: varchar("providerAccountId", { length: 255 }).notNull(),
-    refresh_token: text("refresh_token"),
-    access_token: text("access_token"),
-    expires_at: integer("expires_at"),
-    token_type: varchar("token_type", { length: 255 }),
-    scope: varchar("scope", { length: 255 }),
-    id_token: text("id_token"),
-    session_state: varchar("session_state", { length: 255 }),
-  },
-  (account) => ({
-    compoundKey: primaryKey({
-      columns: [account.provider, account.providerAccountId],
+    "account",
+    {
+        userId: varchar("userId", { length: 255 })
+            .notNull()
+            .references(() => users.id),
+        type: varchar("type", { length: 255 })
+            .$type<AdapterAccount["type"]>()
+            .notNull(),
+        provider: varchar("provider", { length: 255 }).notNull(),
+        providerAccountId: varchar("providerAccountId", {
+            length: 255,
+        }).notNull(),
+        refresh_token: text("refresh_token"),
+        access_token: text("access_token"),
+        expires_at: integer("expires_at"),
+        token_type: varchar("token_type", { length: 255 }),
+        scope: varchar("scope", { length: 255 }),
+        id_token: text("id_token"),
+        session_state: varchar("session_state", { length: 255 }),
+    },
+    (account) => ({
+        compoundKey: primaryKey({
+            columns: [account.provider, account.providerAccountId],
+        }),
+        userIdIdx: index("account_userId_idx").on(account.userId),
     }),
-    userIdIdx: index("account_userId_idx").on(account.userId),
-  })
 );
 
 export const accountsRelations = relations(accounts, ({ one }) => ({
-  user: one(users, { fields: [accounts.userId], references: [users.id] }),
+    user: one(users, { fields: [accounts.userId], references: [users.id] }),
 }));
 
 export const sessions = createTable(
-  "session",
-  {
-    sessionToken: varchar("sessionToken", { length: 255 })
-      .notNull()
-      .primaryKey(),
-    userId: varchar("userId", { length: 255 })
-      .notNull()
-      .references(() => users.id),
-    expires: timestamp("expires", { mode: "date" }).notNull(),
-  },
-  (session) => ({
-    userIdIdx: index("session_userId_idx").on(session.userId),
-  })
+    "session",
+    {
+        sessionToken: varchar("sessionToken", { length: 255 })
+            .notNull()
+            .primaryKey(),
+        userId: varchar("userId", { length: 255 })
+            .notNull()
+            .references(() => users.id),
+        expires: timestamp("expires", { mode: "date" }).notNull(),
+    },
+    (session) => ({
+        userIdIdx: index("session_userId_idx").on(session.userId),
+    }),
 );
 
 export const sessionsRelations = relations(sessions, ({ one }) => ({
-  user: one(users, { fields: [sessions.userId], references: [users.id] }),
+    user: one(users, { fields: [sessions.userId], references: [users.id] }),
 }));
 
 export const verificationTokens = createTable(
-  "verificationToken",
-  {
-    identifier: varchar("identifier", { length: 255 }).notNull(),
-    token: varchar("token", { length: 255 }).notNull(),
-    expires: timestamp("expires", { mode: "date" }).notNull(),
-  },
-  (vt) => ({
-    compoundKey: primaryKey({ columns: [vt.identifier, vt.token] }),
-  })
+    "verificationToken",
+    {
+        identifier: varchar("identifier", { length: 255 }).notNull(),
+        token: varchar("token", { length: 255 }).notNull(),
+        expires: timestamp("expires", { mode: "date" }).notNull(),
+    },
+    (vt) => ({
+        compoundKey: primaryKey({ columns: [vt.identifier, vt.token] }),
+    }),
 );
+
+
+export const appointment = createTable("appointment", {
+    id: serial("id").primaryKey(),
+    userId: varchar("userId", { length: 255 })
+        .references(() => users.id)
+        .notNull(),
+    doctorId: varchar("doctorId", { length: 255 })
+        .references(() => users.id)
+        .notNull(),
+    createdById: varchar("createdById", { length: 255 })
+        .notNull()
+        .references(() => users.id),
+    appointmentDate: date("appointmentDate").notNull(),
+    paymentAmount: real("paymentAmount"),
+    paymentStatus: paymentStatus("paymentStatus").default("pending"),
+    isCancelled: boolean("isCancelled").default(false),
+    createdAt: timestamp("created_at")
+        .default(sql`CURRENT_TIMESTAMP`)
+        .notNull(),
+});
+
+
+
