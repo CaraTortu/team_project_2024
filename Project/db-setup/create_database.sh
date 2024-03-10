2ec2a25ae36e6b198c1d85c64c442e809d51d773
CaraTortu <javierdj@jadk.com>
2024-03-10 20:29:33 +0000

    Add windows scripts

--- Project/db-setup/create_database.sh
+++ Project/db-setup/create_database.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 # Use this script to start a docker container for a local development database
-
-# TO RUN ON WINDOWS: 
-# 1. Install WSL (Windows Subsystem for Linux) - https://learn.microsoft.com/en-us/windows/wsl/install
-# 2. Install Docker Desktop for Windows - https://docs.docker.com/docker-for-windows/install/
-# 3. Open WSL - `wsl`
-# 4. Run this script - `./start-database.sh`
-
-# On Linux and macOS you can run this script directly - `./start-database.sh`
-
 DB_CONTAINER_NAME="gp-system-postgres"
 
 if ! [ -x "$(command -v docker)" ]; then
